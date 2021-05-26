96762091f91a3735e0e91c4b352cb40a4e0683cf
Osman Barrera <Osmanbj2001@gmail.com>
2021-05-26 12:21:56 -0600

    Proyecto final terminado

--- mean-stack-crud-app/src/app/app.module.ts
+++ mean-stack-crud-app/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeCreateComponent } from './components/employee-create/employee-create.component';
 import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
 import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { ApiService } from './service/api.service';
 
+@NgModule({
+  providers: [ApiService]
+})
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,10 +19,12 @@ import { EmployeeListComponent } from './components/employee-list/employee-list.
     EmployeeListComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
